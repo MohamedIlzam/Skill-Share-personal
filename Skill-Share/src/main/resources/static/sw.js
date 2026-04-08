f631b9e1ed460b9ab6b9aa22feeb20cea3385ed9
MohamedIlzam <it24610792@my.sliit.lk>
2026-04-08 21:52:32 +0530

    Add sprint 3 features and tests

---
+++ Skill-Share/src/main/resources/static/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'skillshare-v1';
+const urlsToCache = [
+  '/',
+  '/browse',
+  '/css/pwa.css',
+  '/manifest.json',
+  '/icons/icon-512.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
